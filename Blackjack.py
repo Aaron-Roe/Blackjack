d9e03e8ec77f894a9f11b4681c50b6b843f27eaf
samupti <uptigrovesam@gmail.com>
2025-02-17 10:51:56 -0500

    spaces
    added spaces

--- Blackjack.py
+++ Blackjack.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Card:
     Suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     Ranks = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
@@ -11,6 +12,7 @@ class Card:
     
     def __str__(self):
         return f"{self.rank} of {self.suit}" # Ex Ace of Hearts
+
 
 class Deck:
     def __init__(self, decks = 2):
